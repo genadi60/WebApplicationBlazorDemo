2890ad9abaed10d6566523b21345d32da58d0382
genadi60 <genadimihaylov@gmail.com>
2019-01-19 18:30:50 +0200

    Created Blazor application with server.

---
+++ WebApplicationBlazorDemo.Server/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationBlazorDemo.Shared;
+
+namespace WebApplicationBlazorDemo.Server.Controllers
+{
+    public class AccountController : BaseController
+    {
+        [HttpPost("[action]")]
+        public UserLoginInputModel  Login([FromBody]UserLoginInputModel model)
+        {
+           
+            return model;
+        }
+    }
+}
---
+++ WebApplicationBlazorDemo.Shared/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplicationBlazorDemo.Shared
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
